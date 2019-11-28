23e8b5e119c6909b5add08fec45b50471d6f0cb2
回向明 <33412599+huixiangming@users.noreply.github.com>
2019-11-28 20:49:35 +0800

    a

--- 回向明/src/container/Video.js
+++ 回向明/src/container/Video.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
-import {SearchBar, NavBar, Icon ,Flex} from 'antd-mobile';
+import { NavBar, Icon } from 'antd-mobile';
 
 export default class Video extends Component{
     render(){
+        
         return(
+            
             <div>
                 <NavBar               
             mode="light"
@@ -11,7 +13,21 @@ export default class Video extends Component{
                 <Icon type={'left'} style={{position:'absolute',top:'10px',left:'4%',color:'white'}}/>
                 <span style={{backgroundColor:'#66cccc',color:'white'}}>更多视频</span>
             </NavBar>
+
+            <video src={require('../images/1.ogg')} controls="controls" autoplay="true" loop="true" 
+            style={{width:'100%',height:'50vw',position:'relative',top:'5vw'}}>
+			您的浏览器不支持video
+		</video>
+        <video src={require('../images/1.ogg')} controls="controls" autoplay="true" loop="true" 
+            style={{width:'100%',height:'50vw',position:'relative',top:'15vw'}}>
+			您的浏览器不支持video
+		</video>
+        <video src={require('../images/1.ogg')} controls="controls" autoplay="true" loop="true" 
+            style={{width:'100%',height:'50vw',position:'relative',top:'25vw'}}>
+			您的浏览器不支持video
+		</video>
             </div>
+            
             
         )
     }
